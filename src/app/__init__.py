232fef8c586a2922db3cb2dca63ea630e101d435
Machi-san <maxibengochea67@gmail.com>
2024-05-26 23:32:11 -0400

    templates in root

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,12 +1,14 @@
 #dependencias
-from flask import Flask, Blueprint
+from flask import Flask
 from src.app.config import SECRET_KEY
 from src.app.authentication import auth
+from src.app.whatsapp import wp
 
 #inicializar la app
 def create_app():
   app = Flask(__name__)
   app.config['SECRET_KEY'] = SECRET_KEY
   app.register_blueprint(auth)
+  app.register_blueprint(wp)
   return app
 
