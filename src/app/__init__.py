5e16b78f935ec39de31351efd04be495a1ed1955
Machi-san <maxibengochea67@gmail.com>
2024-07-09 22:08:27 -0400

    delete whatsapp blueprint

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,14 +1,12 @@
 #dependencias
 from flask import Flask
 from src.app.config import SECRET_KEY
-from src.app.authentication import auth
-from src.app.whatsapp import wp
+from src.app.auth import auth
 
 #inicializar la app
 def create_app():
   app = Flask(__name__)
   app.config['SECRET_KEY'] = SECRET_KEY
   app.register_blueprint(auth)
-  app.register_blueprint(wp)
   return app
 
