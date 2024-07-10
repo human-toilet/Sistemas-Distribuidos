5b200ad568a6b9a09b855f4db58a149e1b0a9281
Machi-san <maxibengochea67@gmail.com>
2024-07-09 22:09:12 -0400

    delete folders

--- src/app/authentication/__init__.py
+++
@@ -1,6 +0,0 @@
-#dependencias
-from flask import Blueprint
-
-#crear el blueprint 'auth' e importar las vistas
-auth = Blueprint('auth', __name__, template_folder='templates')
-import src.app.authentication.routes
--- src/app/whatsapp/__init__.py
+++
@@ -1,6 +0,0 @@
-#dependencias
-from flask import Blueprint
-
-#crear el blueprint 'auth' e importar las vistas
-wp = Blueprint('wp', __name__, template_folder='templates')
-import src.app.whatsapp.routes
