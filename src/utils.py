3d7f5d1ed8e5d1b3aec12abb7a91e018fd2c86d9
Machi-san <maxibengochea67@gmail.com>
2024-07-12 04:02:23 -0400

    add get-ip

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,18 @@
 import hashlib
+import socket
 
 def set_id(data: str):
   return str(hashlib.sha1(data.encode()).hexdigest())
+
+def get_ip() -> str:
+  with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    try:
+        #usamos una IP arbitraria que no se encuentra en nuestra red
+        s.connect(('10.254.254.254', 1))
+        ip_local = s.getsockname()[0]
+        
+    except Exception:
+        ip_local = '127.0.0.1'
+        
+    return str(ip_local)
+  
