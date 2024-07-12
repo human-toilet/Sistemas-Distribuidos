c905ee535215cf5edc235da821786fab4d82157a
Machi-san <maxibengochea67@gmail.com>
2024-07-11 20:58:26 -0400

    static port

--- src/app/auth/__init__.py
+++ src/app/auth/__init__.py
@@ -3,7 +3,7 @@ from flask import Blueprint
 from src.code.chord import Server
 
 #inicializar el server
-server = Server(8000)
+server = Server()
 
 #crear el blueprint 'auth' e importar las vistas
 auth = Blueprint('auth', __name__, template_folder='templates')
