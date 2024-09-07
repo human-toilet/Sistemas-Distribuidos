b02a68d3d7748b99b72d4d6553f4821390f27333
P3ND4 <josecarlospendasrodriguez@gmail.com>
2024-09-07 18:48:28 -0400

    validate fix

--- src/visual/auth/forms.py
+++ src/visual/auth/forms.py
@@ -10,19 +10,19 @@ def _validate_field(form, field):
 #formulario de la vista 'register'
 class Register(FlaskForm):
   username = StringField('username', validators=[DataRequired(), Length(1, 16, message='max 16 characters'), _validate_field])
-  number = IntegerField('number', validators=[DataRequired(), NumberRange(min=10000000, message='invalid number (8 digits required)'), _validate_field])
+  number = IntegerField('number', validators=[DataRequired(), NumberRange(min=10000000, message='invalid number (8 digits required)')])
   submit = SubmitField('sign up')
   
 #formulario de la vista 'login'
 class Login(FlaskForm):
   username = StringField('username', validators=[DataRequired(), _validate_field])
-  number = IntegerField('number', validators=[DataRequired(), _validate_field])
+  number = IntegerField('number', validators=[DataRequired()])
   submit = SubmitField('sign in')
   
 #formulario de la vista 'add_contact'
 class AddContact(FlaskForm):
   name = StringField('name', validators=[DataRequired(), Length(1, 16, message='max 16 characters'), _validate_field])
-  number = IntegerField('number', validators=[DataRequired(), NumberRange(min=10000000, message='invalid number (8 digits required)'), _validate_field])
+  number = IntegerField('number', validators=[DataRequired(), NumberRange(min=10000000, message='invalid number (8 digits required)')])
   submit = SubmitField('add contact')
   
 #formulario para enviar mensajes
